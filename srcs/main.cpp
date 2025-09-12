9b19f01381297712d2941febaeba08a53bd0af74
Xiaoyun Xu <xiaxu@f1br6s4.clusters.42paris.fr>
2025-09-12 20:37:18 +0200

    TODO: add test cases that work for 4*4 and 5*5

--- srcs/main.cpp
+++ srcs/main.cpp
@@ -17,7 +17,7 @@
 # define ERASE = "\033[2K\r"
 # define RESET "\033[0m"
 
-int dirs[4][2] = {{0, -1}, {0, 1}, {-1, 0}, {1, 0}};
+int dirs[4][2] = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
 int heuristic = 0, solver = 0;
 std::vector<int> goal;
 bool verbose = false;
@@ -115,6 +115,21 @@ static int heuristicFunction(const std::vector<int>& state, const std::vector<st
 	return manhattan(state, pos, n);
 }
 
+static void printSolution(unsigned long long int cnt, unsigned long long int mx, const std::string& move)
+{
+	std::cout << MAGENTA << BOLD << "Time complexity: " << UNDER << cnt << RESET << MAGENTA << " states selected in total" << std::endl;
+	std::cout << MAGENTA << BOLD << "Size complexity: " << RESET << MAGENTA << "at most " << BOLD << UNDER << mx << RESET << MAGENTA << " states ever represented in memory at the same time" << std::endl;
+	std::cout << MAGENTA << BOLD << "Number of moves to achieve the final state: " << UNDER << move.size() << RESET << std::endl;
+	std::cout << MAGENTA << BOLD << "Solution: " << RESET << MAGENTA << move << RESET << std::endl;
+}
+
+static std::string encode(const std::vector<int>& state)
+{
+	std::string s = "";
+	for (int i : state) s += std::to_string(i) + "$";
+	return s;
+}
+
 bool solve(std::vector<int>& grid, int n)
 {
 	goal = makeGoal(n);
@@ -124,7 +139,7 @@ bool solve(std::vector<int>& grid, int n)
 
 	using Node = std::tuple<int, int, std::vector<int>, int, int, std::string>;
 	std::priority_queue<Node, std::vector<Node>, std::greater<Node>> pq;
-	std::unordered_set<unsigned long long int> vis;
+	std::unordered_set<std::string> vis;
 
 	int h = heuristicFunction(grid, pos, n);
 	int zero = (int)(std::find(grid.begin(), grid.end(), 0) - grid.begin());
@@ -140,17 +155,13 @@ bool solve(std::vector<int>& grid, int n)
 
 		if (state == goal)
 		{
-			std::cout << MAGENTA << BOLD << "Time complexity: " << UNDER << cnt << RESET << MAGENTA << " states selected in total" << std::endl;
-			std::cout << MAGENTA << BOLD << "Size complexity: " << RESET << MAGENTA << "at most " << BOLD << UNDER << mx << RESET << MAGENTA << " states ever represented in memory at the same time" << std::endl;
-			std::cout << MAGENTA << BOLD << "Number of moves to achieve the final state: " << UNDER << move.size() << RESET << std::endl;
-			std::cout << MAGENTA << BOLD << "Solution: " << RESET << MAGENTA << move << RESET << std::endl;
+			printSolution(cnt, mx, move);
 			return true;
 		}
 
-		unsigned long long int num = 0;
-		for (int i : state) num = ((num << 4) | i);
-		if (vis.find(num) != vis.end()) continue;
-		vis.insert(num);
+		std::string s = encode(state);
+		if (vis.find(s) != vis.end()) continue;
+		vis.insert(s);
 
 		int x = z / n, y = z % n;
 		for (int i = 0; i < 4; i++)
@@ -163,15 +174,18 @@ bool solve(std::vector<int>& grid, int n)
 			std::vector<int> next = state;
 			std::swap(next[z], next[dz]);
 			int curh = heuristicFunction(next, pos, n);
-			if (i == 0) move += "U";
-			else if (i == 1) move += "D";
-			else if (i == 2) move += "L";
-			else move += "R";
 			int f;
 			if (solver == 0) f = g + curh + 1;
 			else if (solver == 1) f = g + 1;
 			else f = curh + 1;
-			pq.emplace(f, g + 1, next, dz, curh, move);
+			if (i == 0)
+				pq.emplace(f, g + 1, next, dz, curh, move + "U");
+			else if (i == 1)
+				pq.emplace(f, g + 1, next, dz, curh, move + "D");
+			else if (i == 2)
+				pq.emplace(f, g + 1, next, dz, curh, move + "L");
+			else
+				pq.emplace(f, g + 1, next, dz, curh, move + "R");
 		}
 	}
 	return false;
@@ -180,75 +194,78 @@ bool solve(std::vector<int>& grid, int n)
 static void printUsage(void)
 {
 	std::cout << "Usage: ./n_puzzle [options]\n\n";
-    std::cout << "Options:\n";
-    std::cout << "  --filename <file>        Load puzzle from input file\n";
-    std::cout << "  --random <n>             Generate a random solvable puzzle of size n x n\n";
-    std::cout << "  --heuristic <type>       Choose heuristic function (default: manhattan)\n";
-    std::cout << "                           Options: manhattan, linear_conflict, hamming\n";
-    std::cout << "  --solver <type>          Choose search algorithm (default: a_star)\n";
-    std::cout << "                           Options: a_star, greedy, uniform_cost\n";
-    std::cout << "  --verbose                Print all puzzle states during solving\n";
-    std::cout << "  --help, -h               Show this help message and exit\n";
-    std::cout << "\nExamples:\n";
-    std::cout << " ./n-puzzle --filename puzzle.txt --heuristic linear_conflict --solver greedy\n";
-    std::cout << " ./n-puzzle --random 3 --heuristic manhattan --solver uniform_cost\n";
+	std::cout << "Options:\n";
+	std::cout << "  --filename <file>        Load puzzle from input file\n";
+	std::cout << "  --random <n>             Generate a random solvable puzzle of size n x n\n";
+	std::cout << "  --heuristic <type>       Choose heuristic function (default: manhattan)\n";
+	std::cout << "                           Options: manhattan, linear_conflict, hamming\n";
+	std::cout << "  --solver <type>          Choose search algorithm (default: a_star)\n";
+	std::cout << "                           Options: a_star, greedy, uniform_cost\n";
+	std::cout << "  --help, -h               Show this help message and exit\n";
+	std::cout << "\nExamples:\n";
+	std::cout << " ./n-puzzle --filename puzzle.txt --heuristic linear_conflict --solver greedy\n";
+	std::cout << " ./n-puzzle --random 3 --heuristic manhattan --solver uniform_cost\n";
 }
 
 void readFile(const std::string &filename, int &n, std::vector<int> &grid) {
-    std::ifstream file(filename);
-    if (!file.is_open())
+	std::ifstream file(filename);
+	if (!file.is_open())
 	{
 		std::cerr << "Error: input file open failure" << std::endl;
 		exit(1);
 	}
 
-    std::string line;
-    while (std::getline(file, line))
+	std::string line;
+	while (std::getline(file, line))
 	{
 		std::cout << line << std::endl;
-        if (!line.empty() && line[0] != '#') break;
+		if (!line.empty() && line[0] != '#') break;
 	}
 
-    n = std::stoi(line);
+	n = std::stoi(line);
 
 	while (std::getline(file, line))
-    {
+	{
 		std::cout << line << std::endl;
-        std::stringstream ss(line);
-        int val;
-        while (ss >> val) grid.push_back(val);
-    }
+		std::stringstream ss(line);
+		int val;
+		while (ss >> val) grid.push_back(val);
+	}
 }
 
 void generateRandomPuzzle(int n, std::vector<int> &grid)
 {
-    std::string command = "python case-gen.py " + std::to_string(n);
-    FILE* pipe = popen(command.c_str(), "r");
-    if (!pipe)
+	std::string command = "python case-gen.py " + std::to_string(n);
+	FILE* pipe = popen(command.c_str(), "r");
+	if (!pipe)
 	{
 		std::cerr << "Error: popen failure" << std::endl;
 		exit(1);
 	}
 
 	int count = 0;
-    char buffer[128];
-    std::string line;
+	char buffer[128];
+	std::string line;
 
-    while (fgets(buffer, sizeof(buffer), pipe))
+	while (fgets(buffer, sizeof(buffer), pipe))
 	{
-        line = buffer;
+		line = buffer;
 		std::cout << line;
 		if (!line.empty() && line[0] == '#') continue;
-        std::stringstream ss(line);
-        int val;
-        while (ss >> val)
+		std::stringstream ss(line);
+		int val;
+		while (ss >> val)
 			if (count++ > 0) grid.push_back(val);
-    }
-    pclose(pipe);
+	}
+	std::cout << std::endl;
+	pclose(pipe);
 }
 
 int main(int ac, char** av)
 {
+	std::ios::sync_with_stdio(false);
+	std::cin.tie(0);
+	std::cout.tie(0);
 	if (ac < 2)
 	{
 		printUsage();
@@ -262,36 +279,36 @@ int main(int ac, char** av)
 	for (size_t i = 0; i < args.size(); i++)
 	{
 		if (args[i] == "--filename" && i + 1 < args.size()) inputfile = args[++i];
-        else if (args[i] == "--random" && i + 1 < args.size()) n = std::stoi(args[++i]);
-        else if (args[i] == "--heuristic" && i + 1 < args.size())
+		else if (args[i] == "--random" && i + 1 < args.size()) n = std::stoi(args[++i]);
+		else if (args[i] == "--heuristic" && i + 1 < args.size())
 		{
 			std::string h = args[++i];
-            if (h == "manhattan") heuristic = 0;
-            else if (h == "linear_conflict") heuristic = 1;
-            else if (h == "hamming") heuristic = 2;
-            else
+			if (h == "manhattan") heuristic = 0;
+			else if (h == "linear_conflict") heuristic = 1;
+			else if (h == "hamming") heuristic = 2;
+			else
 			{
-                std::cerr << "Unknown heuristic: " << h << std::endl;
-                return 1;
+				std::cerr << "Unknown heuristic: " << h << std::endl;
+				return 1;
 			}
 		}
-        else if (args[i] == "--solver" && i + 1 < args.size())
+		else if (args[i] == "--solver" && i + 1 < args.size())
 		{
 			std::string s = args[++i];
-            if (s == "a_star") solver = 0;
-            else if (s == "uniform_cost") solver = 1;
-            else if (s == "greedy") solver = 2;
-            else
+			if (s == "a_star") solver = 0;
+			else if (s == "uniform_cost") solver = 1;
+			else if (s == "greedy") solver = 2;
+			else
 			{
-                std::cerr << "Unknown solver: " << s << std::endl;
-                return 1;
-            }
+				std::cerr << "Unknown solver: " << s << std::endl;
+				return 1;
+			}
 		}
 		else if (args[i] == "--help" || args[i] == "-h")
 		{
-            std::cout << "Usage: ./program [--filename <file>] [--random <n>] [--heuristic <type>] [--solver <type>]\n";
-            return 0;
-        }
+			std::cout << "Usage: ./program [--filename <file>] [--random <n>] [--heuristic <type>] [--solver <type>]\n";
+			return 0;
+		}
 	}
 
 	if (inputfile != "") readFile(inputfile, n, grid);
@@ -303,7 +320,6 @@ int main(int ac, char** av)
 	else generateRandomPuzzle(n, grid);
 	// ------------------------- Start timing -----------------------------
 	auto start = std::chrono::high_resolution_clock::now();
-
 	auto check = [&]() -> bool
 	{
 		int cnt = 0;
@@ -319,9 +335,17 @@ int main(int ac, char** av)
 			return ((cnt + r) % 2 == 1);
 		}
 	};
-	if (check())
-		solve(grid, n);
-	else std::cout << RED << "The puzzle is unsolvable" << RESET << std::endl;
+
+	if (n == 3)
+	{
+		if (check()) solve(grid, n);
+		else std::cout << RED << "The puzzle is unsolvable" << RESET << std::endl;
+	}
+	else
+	{
+		if (!solve(grid, n))
+			std::cout << RED << "The puzzle is unsolvable" << RESET << std::endl;
+	}
 
 	// ------------------------- End timing -----------------------------
 	auto end = std::chrono::high_resolution_clock::now();
